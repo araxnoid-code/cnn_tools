6a67faf1ea7d4324d121a771f5eec0d38beca55e
araxnoid-code <araxnoid0@gmail.com>
2026-06-20 06:53:28 +0700

    added adam_optim in conv2d

--- src/tools/method/conv2d.rs
+++ src/tools/method/conv2d.rs
@@ -9,15 +9,61 @@ use rand_distr::{Distribution, Normal};
 
 pub struct Conv2DNonBatch {
     kernel: Array4<f32>,
-    grads_grad: Array4<f32>,
+    kernel_grad: Array4<f32>,
     bias: Array1<f32>,
     bias_grad: Array1<f32>,
     in_channel: usize,
     out_channel: usize,
     kernel_size: usize,
+
+    e: f32,
+    t: usize,
+    v_kernel: Array4<f32>,
+    m_kernel: Array4<f32>,
+    v_bias: Array1<f32>,
+    m_bias: Array1<f32>,
+    b_1: f32,
+    b_2: f32,
 }
 
 impl Conv2DNonBatch {
+    pub fn adam_optim(&mut self, lr: f32) {
+        // weight
+        let m = &self.m_kernel * self.b_1 + (1. - self.b_1) * &self.kernel_grad;
+        let correction_m = &m / (1. - self.b_1.powi(self.t as i32));
+
+        let v = &self.v_kernel * self.b_2 + (1. - self.b_2) * self.kernel_grad.pow2();
+        let correction_v = &v / (1. - self.b_2.powi(self.t as i32));
+
+        self.kernel = &self.kernel.view() - lr / (self.e + correction_v.sqrt()) * correction_m;
+        self.m_kernel = m;
+        self.v_kernel = v;
+
+        // bias
+        let m = &self.m_bias * self.b_1 + (1. - self.b_1) * &self.bias_grad;
+        let correction_m = &m / (1. - self.b_1.powi(self.t as i32));
+
+        let v = &self.v_bias * self.b_2 + (1. - self.b_2) * self.bias_grad.pow2();
+        let correction_v = &v / (1. - self.b_2.powi(self.t as i32));
+
+        self.bias = &self.bias.view() - lr / (self.e + correction_v.sqrt()) * correction_m;
+        self.m_bias = m;
+        self.v_bias = v;
+
+        self.t += 1;
+    }
+
+    pub fn zero_grad(&mut self) {
+        self.kernel_grad = Array4::zeros([
+            self.out_channel,
+            self.in_channel,
+            self.kernel_size,
+            self.kernel_size,
+        ]);
+
+        self.bias_grad = Array1::<f32>::zeros([self.out_channel]);
+    }
+
     pub fn new(in_channel: usize, out_channel: usize, kernel_size: usize) -> Conv2DNonBatch {
         let len = out_channel * in_channel * kernel_size * kernel_size;
 
@@ -32,12 +78,21 @@ impl Conv2DNonBatch {
                 (0..len).map(|_| normal.sample(&mut rng)).collect(),
             )
             .unwrap(),
-            grads_grad: Array4::<f32>::zeros([out_channel, in_channel, kernel_size, kernel_size]),
+            kernel_grad: Array4::<f32>::zeros([out_channel, in_channel, kernel_size, kernel_size]),
             bias: Array1::<f32>::zeros([out_channel]),
             bias_grad: Array1::<f32>::zeros([out_channel]),
             in_channel,
             kernel_size,
             out_channel,
+
+            e: 0.00000001,
+            t: 1,
+            v_kernel: Array4::<f32>::zeros([out_channel, in_channel, kernel_size, kernel_size]),
+            m_kernel: Array4::<f32>::zeros([out_channel, in_channel, kernel_size, kernel_size]),
+            v_bias: Array1::<f32>::zeros([out_channel]),
+            m_bias: Array1::<f32>::zeros([out_channel]),
+            b_1: 0.999,
+            b_2: 0.9,
         }
     }
 
@@ -77,7 +132,7 @@ impl Conv2DNonBatch {
                             .unwrap();
 
                         let d_kernel = slice_matrix * *grad;
-                        self.grads_grad
+                        self.kernel_grad
                             .slice_mut(s![
                                 out_kernel_idx..out_kernel_idx + 1,
                                 channel_idx..channel_idx + 1,
